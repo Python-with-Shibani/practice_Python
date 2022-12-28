00b0cc054d900394e58fce378331415c8df8a69a
Shibani Thakur <121497888+Python-with-Shibani@users.noreply.github.com>
2022-12-28 10:32:14 +0530

    Add files via upload

---
+++ input_function.py
@@ -0,0 +1,18 @@
+age = input()
+print(f"Age is {age}")
+age = input("Enter your age: ")
+print(f"Age is {age}")
+new_age = int(age) + 5
+print(f"Your new age is {new_age}")
+
+print(type (age))
+
+# new_age = age + str(5)   does not work
+
+age = int(input("Enter age : "))
+new_age = age + 5
+print("new  ag: ", new_age)
+
+intrest_rate = float(input("Enter intrest: "))
+print(type(intrest_rate))
+print(intrest_rate)
