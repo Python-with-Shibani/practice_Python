14d802ddc483e691aed2284e5732c819e22cdc36
Shibani Thakur <121497888+Python-with-Shibani@users.noreply.github.com>
2022-12-29 18:49:12 +0530

    Add files via upload

---
+++ input_practice.py
@@ -0,0 +1,40 @@
+# promble 3
+# number = int(input("Enter a number: "))
+# result = number * 10 / 10
+# print (result)
+
+# problem 4
+#user_name = input("what's your name:")
+#user_age = input("what's your age:")
+#print (f"your name is {user_name} and age is {user_age}")
+#print("your name is " + user_name "and age is " + int(user_age))
+
+# problem 5
+#num1 = int(input("Enter a number: "))
+#num2 = int(input("Enter a number: "))
+#result = num1 + num2
+#print(result)
+
+#message= input("how namy days would you like to convert to a year?")
+#result= int(message)/365
+#print(result)
+
+#a = int(input("a:"))
+#b = int(input("b:"))
+#add = a+b
+#mul = a*b
+#div = a/b
+#print("addition is: ", add)
+#print("multiplication is: ", mul)
+#print("division: ", div)
+
+
+#a = input("enter value 1: ")
+#b = input("enter value 2: ")
+#c = input("enter value 3: ")
+#result = a, b, c
+#print("result is " + a, b, c)
+
+val1, val2, val3 = input("Enter three values ").split()
+print(val1, val2, val3)
+
